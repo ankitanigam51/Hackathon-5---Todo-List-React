1f3dff4ddd4738f4842129f7bb5fd04d4992141a
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-13 17:28:48 +0530

    assgnmnt

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,14 @@ function App()
 {
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<input type="text" id="task"></input>
+		<button type="button" id="btn" onclick="add()">Add Items</button>
+	<ul>
+		<li className="list">Item 1</li>
+		<li className="list">Item 2</li>
+		<li className="list">Item 3</li>
+	</ul>
+	<button type="button" id="clear-list" onclick="removeAll()">Clear Items</button>
 	</div>
 	);
 }
